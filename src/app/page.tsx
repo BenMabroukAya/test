9600873312c0b88c49d1def08682a16b92b5cda0
aya <aya.benmabrouk@isimg.tn>
2024-06-04 17:00:38 +0100

    add descriptio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,8 @@ const Page = () => {
         <img src="/image.png" alt="Logo Énergie Solaire" style={{ width: '150px', height: 'auto' }} />
       </header>
       <main>
-        <center><h1>Énergie Solaire</h1></center>
+        
+        <center><h1>Énergie Solaire</h1>
         <p>
           L'énergie solaire est une source d'énergie renouvelable qui provient du rayonnement du soleil. 
           Elle peut être captée et convertie en chaleur ou en électricité à l'aide de technologies comme 
@@ -16,7 +17,35 @@ const Page = () => {
           Les avantages de l'énergie solaire incluent une réduction des émissions de gaz à effet de serre, 
           une dépendance énergétique réduite, et une ressource inépuisable et gratuite. Cependant, 
           l'intermittence et le coût initial d'installation sont des défis à considérer.
+        </p></center>
+
+        <center><h2>Rôle de l'énergie Solaire</h2>
+        <p>
+        Les panneaux solaires produisent ce qu'on appelle une énergie verte. Une énergie bénéfique pour notre 
+        environnement car elle ne rejette aucun produit polluant comme le CO2 et donc réduit notre empreinte 
+        carbone. De plus, elle ralentit considérablement le réchauffement climatique.
+        </p></center>
+
+        <center><h2>Types de l'énergie Solaire</h2></center>
+        <p>
+          <ul>
+
+            <li>Energie thermique :</li> < br />
+            L’énergie solaire thermique est une technologie qui consiste à capter les rayons solaires pour 
+            produire de la chaleur ou de l’eau chaude.< br />
+
+            < br /><li>Energie thermique</li>< br />
+            L’énergie solaire photovoltaïque est une énergie électrique renouvelable et inépuisable à 
+            l’échelle humaine, qui ne produit pas de CO2.
+            Celle-ci utilise des panneaux solaires ou des centrales photovoltaïques, composés de cellules 
+            photovoltaïques qui vont capter les rayons solaires.
+          </ul>
         </p>
+
+
+        <center><h2>Où se trouve l'énergie solaire?</h2>
+        <p>L'énergie solaire est utilisée essentiellement pour deux usages : la production d'électricité (énergie 
+          solaire photovoltaïque ou énergie solaire thermodynamique) ou la production de chaleur (énergie solaire thermique).</p></center>
       </main>
     </div>
   );
